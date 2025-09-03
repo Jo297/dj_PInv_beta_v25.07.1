c506d59c0c4b538dd5e5aac1c1bc8da6d73dd0a1
Jo297 <39516057+Jo297@users.noreply.github.com>
2025-09-03 23:11:34 +0530

    update settings

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -89,7 +89,10 @@ LOGOUT_REDIRECT_URL = 'login'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True          # Neon requires SSL
+            )
     }
 else:    
     DATABASES = {
